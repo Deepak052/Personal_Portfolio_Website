b8b4f0673472491771321958db68f418cefa7cdd
Deepak Mehta <dmehta052@gmail.com>
2024-06-24 20:01:18 +0530

    first commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,125 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/Avatar.png";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }} data-aos="fade-right">
+              <span className="primary-header"> ABOUT </span> ME
+            </h1>
+            <p className="home-about-body" data-aos="fade-up">
+              <br />
+              I'm a <i className="primary-header">
+                Self-Taught Web Developer
+              </i>{" "}
+              who loves solving problems and building Websites.
+              <br />
+              <br />I am passionate about{" "}
+              <i className="primary-header">technologies</i> and love to learn
+              new things.
+              <br />
+              <br />I enjoy learning
+              <i>
+                <i className="primary-header"> new skills and implementing</i>them in real 
+                life!
+              </i>
+              <br />
+              <br />
+              Currently expanding my portfolio by building more{" "}
+              <i className="primary-header">projects </i>projects that I can add
+              here.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img
+                data-aos="fade-left"
+                src={myImg}
+                className="img-fluid"
+                alt="avatar"
+              />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1 data-aos="fade-right">
+              <span className="primary-header">CONNECT </span> WITH ME
+            </h1>
+            <p data-aos="fade-left">Feel free to connect with me</p>
+            <ul className="home-about-social-links" data-aos="fade-up">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Deepak052"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="github"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              {/*<li className="social-icons">
+                <a
+                  href="https://twitter.com/kumardotexe"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="twitter"
+                >
+                  <AiOutlineTwitter />
+                </a>
+  </li>*/}
+              <li className="social-icons">
+                <a
+                  href="mailto:dmehta052@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="email"
+                >
+                  <AiOutlineMail />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/deepakmehta052/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                  aria-label="linkedin"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/_.deepak_.mehta._/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                  aria-label="instagram"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
